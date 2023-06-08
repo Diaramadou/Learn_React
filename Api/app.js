a64363fdea965d8399b851ca49b283e5926b5905
Diarassouba Mamadou <couda.dm@gmail.com>
2023-06-08 13:31:39 +0000

    demo

--- Api/app.js
+++ Api/app.js
@@ -1,4 +1,7 @@
 import Http from 'http'
+import qs from 'node:querystring'
+import fs  from 'fs'
+import path from 'path'
 
 const server = Http.createServer((req, res)=>{
   // console.log(req.method)    
@@ -6,15 +9,17 @@ const server = Http.createServer((req, res)=>{
   if (req.url === '/api/auth/register' && req.method === 'POST') {
     let body = ""
     req.on("data",(chunck)=>{
-      console.log(chunck)
       body+= chunck
     })
     req.on("end",()=>{
-     
-      const buff = Buffer.from(body)
-      const json = buff.toJSON()
+      console.log(body) 
+      body = JSON.parse(body)
+      let oldData = fs.readFileSync(path.join("Api","data","user.json"), {'encoding': "utf-8"})
+      oldData = JSON.parse(oldData)
+      oldData.push(body)
+      
+      fs.writeFileSync(path.join("Api","data","user.json"), JSON.stringify(oldData), {'encoding': "utf-8"})
 
-      console.log(json)
   })
 }})
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,6 @@ function Register()
             <h3>Inscription</h3>
            <input type="text" className="form-control" placeholder="Nom" /><br />
            <input type="prenom" className="form-control" placeholder="Prenom" /> <br />
-
            <input type="email" className="form-control" placeholder="Enter un mail" /><br />
            <input type="password" className="form-control" placeholder="Entrer le mot de passe" /> <br />
            <button type="submit">Inscription</button>
